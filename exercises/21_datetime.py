660fdcf25338419eedfacb55c581e5b72e83800d
Esteban Solorzano Taborda <estebansolorzano27@gmail.com>
2020-08-18 19:27:20 -0500

    Datetime exercises

--- exercises/21_datetime.py
+++ exercises/21_datetime.py
@@ -1,8 +1,26 @@
 """
+Convert the following string dates to datetime format:
+
+December 1th, 2015 15:30
+5 June 2018 5pm
+2020/9/13 00:05:06
 """
 
 """
+Using the operations between dates, find out what is the difference
+in days, months and years from January 1, 2000 to today
 """
 
 """
+Try some of the differences between datetime and dateutil.
+
+For example:
+
+relativedelta
+parser.parse
+"""
+
+"""
+Get all the dates from the Monday of the current month, you can use for
+loop and operations between dates
 """
